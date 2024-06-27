fea785ca2ae7601ae2be31d14de54861a44d1927
john <gomezsjalex@gmail.com>
2024-06-26 23:09:12 -0500

    Division de pantalla con dos div en el HTML y cambio de color de fondo de pantalla en cada div

--- javascript.js
+++ javascript.js
@@ -5,12 +5,11 @@ const inferior = document.getElementById('inferior');
 const crearColor = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 
 superior.addEventListener('click',()=>{        
-        let color ="#";
-        for(let i =0; i<6; i++){
-            color +=crearColor[randomNumber()]
-        }
-        superior.style.backgroundColor = color;
-            
+    let color ="#";
+    for(let i =0; i<6; i++){
+        color +=crearColor[randomNumber()]
+    }
+    superior.style.backgroundColor = color;
 })
 
 inferior.addEventListener('click',()=>{        
