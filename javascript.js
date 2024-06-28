2ba92508c409abe96d74486a8e0d9fce6777547e
john <gomezsjalex@gmail.com>
2024-06-27 22:56:01 -0500

    Utilizando el evento click desde HTML para el cambio de color

--- javascript.js
+++ javascript.js
@@ -4,21 +4,21 @@ const inferior = document.getElementById('inferior');
 
 const crearColor = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
 
-superior.addEventListener('click',()=>{        
+function cambioColorS(){       
     let color ="#";
     for(let i =0; i<6; i++){
         color +=crearColor[randomNumber()]
     }
     superior.style.backgroundColor = color;
-})
+}
 
-inferior.addEventListener('click',()=>{        
+function cambioColorI(){        
     let color ="#";
     for(let i =0; i<6; i++){
         color +=crearColor[randomNumber()]
     }
     inferior.style.backgroundColor = color;    
-})
+}
 
 function randomNumber(){
     return Math.floor(Math.random() * crearColor.length);
