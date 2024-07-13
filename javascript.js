cc645b2f8cd3f84cf3b9eebf9980967ba82cc6de
JAGS026 <johngomez18@gmail.com>
2024-07-12 22:12:43 -0500

    Se da formato al documento (ctrl+shift+i)

--- javascript.js
+++ javascript.js
@@ -2,24 +2,24 @@ const superior = document.getElementById('superior');
 const inferior = document.getElementById('inferior');
 
 
-const crearColor = [1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"];
+const crearColor = [1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
-function cambioColorS(){       
-    let color ="#";
-    for(let i =0; i<6; i++){
-        color +=crearColor[randomNumber()]
+function cambioColorS() {
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += crearColor[randomNumber()]
     }
     superior.style.backgroundColor = color;
 }
 
-function cambioColorI(){        
-    let color ="#";
-    for(let i =0; i<6; i++){
-        color +=crearColor[randomNumber()]
+function cambioColorI() {
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += crearColor[randomNumber()]
     }
-    inferior.style.backgroundColor = color;    
+    inferior.style.backgroundColor = color;
 }
 
-function randomNumber(){
+function randomNumber() {
     return Math.floor(Math.random() * crearColor.length);
 }
