a335d2e3c5f4edec2a62d63e3e5b6caa725891e5
John <johngomez18@gmail.com>
2025-05-19 23:36:30 -0500

    se crea desde JS un elemento de Html, al dar click en cualquier parte de la pantalla y de la división se crea un cuadro y cambia de color al dar nuevame click

--- javascript.js
+++ javascript.js
@@ -3,5 +3,19 @@ function crearColor(element) {
     let randomColor = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
 
     element.style.backgroundColor = randomColor;
+
+
+    let cuadroColor = "#" + Math.floor(Math.random() *0xFFFFFF).toString(16).padStart(6, '0');
+
+
+    let cuadro = element.querySelector('.cuadro');
+    if (!cuadro) {
+        cuadro = document.createElement('div');
+        cuadro.className = 'cuadro';
+        element.appendChild(cuadro);
+    }
+
+
+    cuadro.style.backgroundColor = cuadroColor;
 }
 
