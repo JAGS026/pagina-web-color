771dafcaff53391bcb9934e927d3a4e27721a0b1
John <johngomez18@gmail.com>
2025-05-28 21:02:48 -0500

    Eliminacion del style el .cuadro y se crea  como un objeto en el JavaScript

--- javascript.js
+++ javascript.js
@@ -1,21 +1,25 @@
 function crearColor(element) {
-
     let randomColor = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
-
     element.style.backgroundColor = randomColor;
 
-
-    let cuadroColor = "#" + Math.floor(Math.random() *0xFFFFFF).toString(16).padStart(6, '0');
-
-
     let cuadro = element.querySelector('.cuadro');
+
     if (!cuadro) {
         cuadro = document.createElement('div');
-        cuadro.className = 'cuadro';
+
         element.appendChild(cuadro);
+
+        Object.assign(cuadro.style, {
+            position: 'absolute',
+            width: '100px',
+            height: '100px',
+            backgroundColor: 'black',
+            border: '2px solid black',
+            boxShadow: '0 0 5px rgba(0,0,0,0.3)',
+            transition: 'background-color 0.5s ease'
+        });
     }
 
-
+    let cuadroColor = '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16).padStart(6, '0');
     cuadro.style.backgroundColor = cuadroColor;
 }
-
